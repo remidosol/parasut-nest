163d1cb7a9f0f0dc89c28c6b033ee16d91d2c0be
remidosol <ibrahim.aksut.dev@gmail.com>
2025-05-15 04:47:16 +0300

    refactor: remove Banks enum from ParasutEnvironment

--- lib/parasut.enum.ts
+++ lib/parasut.enum.ts
@@ -6,34 +6,6 @@ export enum ParasutEnvironment {
   PRODUCTION = "PRODUCTION",
 }
 
-export enum Banks {
-  ALBARAKA = "0203",
-  AKBANK = "0046",
-  BANKASYA = "0208",
-  DENIZBANK = "0134",
-  FINANSBANK = "0111",
-  GARANTI = "0062",
-  HALKBANK = "0012",
-  HSBC = "0123",
-  ISBANK = "0064",
-  ING = "0099",
-  KUVEYTTURK = "0205",
-  ODEABANK = "0146",
-  SEKERBANK = "0059",
-  TEBBANK = "0032",
-  TFKB = "0206",
-  VAKIFBANK = "0015",
-  YKB = "0067",
-  ZIRAATBANK = "0010",
-  ICBC = "0109",
-  PAPARA = "6002",
-  TURKPARA = "6001",
-  ININAL = "6004",
-  VAKIFKATILIM = "0210",
-  ZIRAATKATILIM = "0209",
-  LYDIANS = "0890",
-}
-
 export enum GrantType {
   AUTHORIZATION_CODE = "authorization_code",
   PASSWORD = "password",
